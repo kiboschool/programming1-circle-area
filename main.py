c40535671f70d16e5c55f815879aa51b7928bbde
Rob Cobb <rwcobbjr@gmail.com>
2022-09-05 22:05:56 -0600

    add solution

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 # Write a function that calculates the area of a circle.
-# See the Instructions tab for more
+import math
+
+def area_circle(radius):
+    return math.pi * radius ** 2 
+
+# manual testing
+print(area_circle(4)) # should print ~50.27
+print(area_circle(22)) # should print ~1520.53
+print(area_circle(12.3)) # should print ~475.29
